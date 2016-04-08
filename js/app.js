a718a257a2fb62bb22628e3092a29afba170940b
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-08 08:55:33 -0700

    Added fire animation

--- js/app.js
+++ js/app.js
@@ -12,8 +12,16 @@ $(document).ready(function(){
 	.mousedown(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-	})
+		$('.hadouken').finish().show()
+			.animate(
+				{"left": "1020px"}, 
+				500, function(){
+					$(this).hide();
+					$(this).css("left", "520px");
+				}
+			);
+		}
+	)
 	.mouseup(function(){
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
