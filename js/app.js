1f1b3c9c000ea3ff973fbee61aad28e40901c736
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-12 00:56:34 -0700

    Refactored

--- js/app.js
+++ js/app.js
@@ -1,60 +1,57 @@
 $(document).ready(function(){
-	$('.ryu').mouseenter(function(){
-		$('.ryu-still').hide();
-		$('.ryu-cool').hide();
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').hide();
-		$('.hadouken').hide();
-		$('.ryu-cool').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function(){
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-cool').hide();
-		$('.ryu-still').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
-			.animate(
-				{"left": "1020px"}, 
-				500, function(){
-					$(this).hide();
-					$(this).css("left", "520px");
-				}
-			);
-		}
-	)
-	.mouseup(function(){
-		$('.ryu-throwing').hide();
-		$('.ryu-cool').hide();
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	});
+    $('.ryu').mouseenter(function(){
+        hidePoses();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function(){
+        hidePoses();
+        $('.hadouken').hide();
+        $('.ryu-still').show();
+    })
+    .mousedown(function(){
+        playHadouken();
+        hidePoses();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show()
+            .animate(
+                {"left": "1020px"}, 
+                500, function(){
+                    $(this).hide();
+                    $(this).css("left", "520px");
+                }
+            );
+        }
+    )
+    .mouseup(function(){
+        hidePoses();
+        $('.ryu-ready').show();
+    });
 
-	$(document).keydown(function(e){
-		if(e.which === 88){
-			$('.ryu-ready').hide();
-			$('.ryu-still').hide();
-			$('.ryu-throwing').hide();
-			$('.ryu-cool').show();
-		}
-	});
-	$(document).keyup(function(e){
-		if(e.which === 88){
-			$('.ryu-cool').hide();
-			$('.ryu-ready').hide();
-			$('.ryu-throwing').hide();
-			$('.ryu-still').show();
-		}
-	});
+    $(document).keydown(function(e){
+        if(e.which === 88){
+            hidePoses();
+            $('.ryu-cool').show();
+        }
+    });
+    $(document).keyup(function(e){
+        if(e.which === 88){
+            hidePoses();
+            $('.ryu-still').show();
+        }
+    });
 });
 
+//Shows the Hadouken fire effect
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
+}
+
+//Hides all images before displaying the needed one.
+function hidePoses(){
+    $('.ryu-cool').hide();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-still').hide();
 }
